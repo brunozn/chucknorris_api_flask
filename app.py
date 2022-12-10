d9854d4ab839410d4552444bb5f4d939aa0c1f54
bruno <bruno.dias@vortexz.com.br>
2022-12-10 11:03:18 -0300

    Colocando docstring e padronizando a indetação

--- app.py
+++ app.py
@@ -10,8 +10,10 @@ URL_BASE = 'https://api.chucknorris.io/jokes'
 def version():
   return __version__
 
+
 @app.route('/api/jokes/random', methods=["GET"])
 def get_jokes_random():
+    '''Retorna uma piada aleatória'''
     response = requests.get(URL_BASE + '/random')
     dados = response.json()
     if response:
@@ -22,25 +24,27 @@ def get_jokes_random():
 
 @app.route('/api/jokes/category/<category>', methods=["GET"])
 def get_jokes_per_category(category):
-  url = URL_BASE + '/random?category=' + category
-  response = requests.get(url)
-  if response.status_code == 404:
-    return jsonify({'mensagem': 'Categoria não existe', "status": 404})
-  data = response.json()
-  return jsonify(data)
+    '''Retorna uma piada de acordo com a categoria passada'''
+    url = URL_BASE + '/random?category=' + category
+    response = requests.get(url)
+    if response.status_code == 404:
+        return jsonify({'mensagem': 'Categoria não existe', "status": 404})
+    data = response.json()
+    return jsonify(data)
 
 
 @app.route('/api/jokes/filter', methods=["GET"])
 def get_jokes_search():
-  search = request.args['search']
-  limit = request.args.get('limit', default=5, type=int)
-  params = {'query': search}
-  url = URL_BASE + '/search'
-  response = requests.get(url, params)
-  data = response.json()
-  if data['total'] == 0:
-    return jsonify({'mensagem': 'Não existe nenhuma piada com o filtro utilizado', "status": 404})
-  return jsonify({'status': 200, 'limit': limit}, data['result'][:limit])
+    '''Retorna piadas filtradas pela query e quantidade é limitada pelo parametro limit, que caso não seja passado tem valor padrão 5'''
+    search = request.args['search']
+    limit = request.args.get('limit', default=5, type=int)
+    params = {'query': search}
+    url = URL_BASE + '/search'
+    response = requests.get(url, params)
+    data = response.json()
+    if data['total'] == 0:
+        return jsonify({'mensagem': 'Não existe nenhuma piada com o filtro utilizado', "status": 404})
+    return jsonify({'status': 200, 'limit': limit}, data['result'][:limit])
   
   
 if __name__ == '__main__':
