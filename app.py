3ae61b9a3bc8eec8f9db787b93ea209f0f14b8e1
bruno <bruno.dias@vortexz.com.br>
2022-12-10 02:45:38 -0300

    api que retorna uma piada para a categoria escolhida

--- app.py
+++ app.py
@@ -18,6 +18,17 @@ def get_jokes_random():
         return jsonify(dados, {"status": 200})
     else:
         return jsonify({'mensagem': 'Erro ao consumir a API', "status": 404})
+
+
+@app.route('/api/jokes/category/<category>', methods=["GET"])
+def get_jokes_per_category(category):
+  url = URL_BASE + '/random?category=' + category
+  response = requests.get(url)
+  if response.status_code == 404:
+    return jsonify({'mensagem': 'Categoria não existe', "status": 404})
+  data = response.json()
+  return jsonify(data)
+  
   
 if __name__ == '__main__':
     app.run()
