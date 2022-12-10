043e9804d3d1a30dc27c16c2a846a4e770cc8db8
bruno <bruno.dias@vortexz.com.br>
2022-12-10 02:27:14 -0300

    API que retorna uma piada aleatoria

--- app.py
+++ app.py
@@ -2,12 +2,22 @@ from flask import Flask, jsonify, request
 import requests
 
 __version__ = '0.0.1'
-
 app = Flask(__name__)
+
+URL_BASE = 'https://api.chucknorris.io/jokes'
 
 @app.get('/_about')
 def version():
   return __version__
+
+@app.route('/api/jokes/random', methods=["GET"])
+def get_jokes_random():
+    response = requests.get(URL_BASE + '/random')
+    dados = response.json()
+    if response:
+        return jsonify(dados, {"status": 200})
+    else:
+        return jsonify({'mensagem': 'Erro ao consumir a API', "status": 404})
   
 if __name__ == '__main__':
     app.run()
