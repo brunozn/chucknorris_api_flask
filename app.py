c7d0aecb73d3ecdecd52eaba6ca20462efe274f7
bruno <bruno.dias@vortexz.com.br>
2022-12-10 03:04:44 -0300

    api para filtrar por uma query

--- app.py
+++ app.py
@@ -28,6 +28,19 @@ def get_jokes_per_category(category):
     return jsonify({'mensagem': 'Categoria não existe', "status": 404})
   data = response.json()
   return jsonify(data)
+
+
+@app.route('/api/jokes/filter', methods=["GET"])
+def get_jokes_search():
+  search = request.args['search']
+  limit = request.args.get('limit', default=5, type=int)
+  params = {'query': search}
+  url = URL_BASE + '/search'
+  response = requests.get(url, params)
+  data = response.json()
+  if data['total'] == 0:
+    return jsonify({'mensagem': 'Não existe nenhuma piada com o filtro utilizado', "status": 404})
+  return jsonify({'status': 200, 'limit': limit}, data['result'][:limit])
   
   
 if __name__ == '__main__':
